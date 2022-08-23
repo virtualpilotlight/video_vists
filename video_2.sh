84c770a54a9696d91c6c03ca181c9eaeb727cb09
Omega X Rae <omega.x.rae@gmail.com>
2022-08-22 20:26:48 -0700

    updated video_2.sh linux syntax only

--- video_2.sh
+++ video_2.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-echo welcome
+sensible-browser https://youtu.be/9d9jG4qGbZo
+echo welcome!
+sensible-browser https://www.urbandictionary.com/define.php?term=welcome%20back%20to%20my%20channel
+echo https://www.youtube.com/user/whilykitt/about
+sensible-browser https://en.wikipedia.org/wiki/Number_Two
+echo 2
+sensible-browser https://en.wikipedia.org/wiki/Recap_sequence
+echo previously on
+sensible-browser https://en.wikipedia.org/wiki/Script_kiddie
+echo きらきらキラー
+sensible-browser https://youtu.be/SUSoXbY-VHQ
+echo Accompaniment
+sensible-browser https://en.wikipedia.org/wiki/Losing_Sleep
+echo Z\? 
+sensible-browser https://en.wikipedia.org/wiki/Long
+echo How long is a long form video\?
+sensible-browser https://en.wikipedia.org/wiki/Integer_\(computer_science\)#Long_long
+echo learn C, learn C++!!!
+sensible-browser https://en.wikipedia.org/wiki/Toaster_Strudel
+echo samefood!
+sensible-browser https://en.wikipedia.org/wiki/Nothing%27s_Gonna_Stop_Me_Now_\(Samantha_Fox_song\)
+echo it\'s a bop!
+sensible-browser https://en.wikipedia.org/wiki/Garbage_\(band\)
+echo anyone remeber Behind the Music\?
+sensible-browser https://en.wikipedia.org/wiki/Syntax_\(programming_languages\)
+echo is there an echo in here\?
+sensible-browser https://en.wikipedia.org/wiki/Syntax_error
+echo It\'s kinda like being Dick Legzia

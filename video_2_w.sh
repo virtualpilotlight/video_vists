3fbf7cedaacd07422738f252d7af1c0470c0df87
Omega X Rae <omega.x.rae@gmail.com>
2022-08-23 02:20:24 -0700

    syntax for windows updated

---
+++ video_2_w.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+start firefox https://youtu.be/9d9jG4qGbZo
+echo welcome!
+start firefox https://www.urbandictionary.com/define.php?term=welcome%20back%20to%20my%20channel
+echo https://www.youtube.com/user/whilykitt/about
+start firefox https://en.wikipedia.org/wiki/Number_Two
+echo 2
+start firefox https://en.wikipedia.org/wiki/Recap_sequence
+echo previously on
+start firefox https://en.wikipedia.org/wiki/Script_kiddie
+echo きらきらキラー
+start firefox https://youtu.be/SUSoXbY-VHQ
+echo Accompaniment
+start firefox https://en.wikipedia.org/wiki/Losing_Sleep
+echo Z\? 
+start firefox https://en.wikipedia.org/wiki/Long
+echo How long is a long form video\?
+start firefox https://en.wikipedia.org/wiki/Integer_\(computer_science\)#Long_long
+echo learn C, learn C++!!!
+start firefox https://en.wikipedia.org/wiki/Toaster_Strudel
+echo samefood!
+start firefox https://en.wikipedia.org/wiki/Nothing%27s_Gonna_Stop_Me_Now_\(Samantha_Fox_song\)
+echo it\'s a bop!
+start firefox https://en.wikipedia.org/wiki/Garbage_\(band\)
+echo anyone remeber Behind the Music\?
+start firefox https://en.wikipedia.org/wiki/Syntax_\(programming_languages\)
+echo is there an echo in here\?
+start firefox https://en.wikipedia.org/wiki/Syntax_error
+echo It\'s kinda like being Dick Legzia

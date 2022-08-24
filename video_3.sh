69a01727d823fe4dbc67abb8c4f1c6d75f003b37
virtualpilotlight <omega.x.rae@gmail.com>
2022-08-24 07:07:15 -0700

    counter.sh tested

--- video_3.sh
+++ video_3.sh
@@ -5,53 +5,154 @@
 # track 1 is 3:59 
 # track 2 is 4:03
 # I need like libraries or something anything to sort sets of related data
+COUNTER=0
+
+sensible-browser https://www.baeldung.com/linux/bash-script-counter
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://tiswww.case.edu/php/chet/bash/bashtop.html
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://www.gnu.org/software/bash/
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://www.gnu.org/
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://git-scm.com/book/en/v2/Git-Branching-Basic-Branching-and-Merging
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Control_flow
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Computer_science
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/For_loop#Loop_counters
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Comedy
+printf "%d\n" $COUNTER
+let COUNTER++
 echo funny stuff
 sensible-browser https://en.wikipedia.org/wiki/Speed_Racer
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Speed
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Clock_rate
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Microprocessor
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Command-line_interface
-sensible-browser https://imgflip.com/i/6qyvjx # peek performance 
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://imgflip.com/i/6qyvjx # peek performance
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Monitor
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://imdownwith89.files.wordpress.com/2014/12/nsync_pop.gif # pop gif
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://frinkiac.com/video/S07E07/paBi_oMVmfiYbQ4eKofB9T-G9EU=.gif #tab gif
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://www.mozilla.org/en-US/
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Causality
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://i.imgur.com/h8KNoLD.gif # correct gif
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://c.tenor.com/gRjRUZyT4ggAAAAC/fucking-magnets-how-do-they-work-reaction.gif #ICP gif
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Shebang_\(Unix\)
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://i.imgur.com/G0yLqBL.gif # house gif 
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Zebra_\(medicine\)
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Migraine
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Synesthesia
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Johnny_Mnemonic_\(film\)
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://dirkgently.fandom.com/wiki/Pararibulitis
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Fibromyalgia
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/American_Sign_Language
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Alphabet
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Ultra_high_frequency
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/UHF_\(film\)
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://media.giphy.com/media/FmSATvDWOLiBq/giphy.gif #UHF gif
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://www.urbandictionary.com/define.php?term=VPL
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://www.virtualpilotlight.com/
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://homestarrunner.com/sbemails/84-kids-book
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Homestar_Runner
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Howdy_Doody
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://www.themarysue.com/community-season-5x09/
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://www.themarysue.com/wp-content/uploads/2014/03/annieabedhowdycommunity.gif #howdy gif
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Howdy
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Facebook
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/YouTube
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Cybersex_trafficking
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Streaming_media
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://en.wikipedia.org/wiki/Livestreaming
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser # accompanyment placeholder 
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser # placeholder for video link

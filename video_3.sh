9db82be1267c48c667b74611a71861db8e7f10f6
virtualpilotlight <omega.x.rae@gmail.com>
2022-08-24 04:53:27 -0700

    draft script for video_3.sh

---
+++ video_3.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# this is a comment
+# left off af 2:09 timestamp on video
+# track 1 is 3:59 
+sensible-browser https://en.wikipedia.org/wiki/Control_flow
+sensible-browser https://en.wikipedia.org/wiki/Computer_science
+sensible-browser https://en.wikipedia.org/wiki/For_loop#Loop_counters
+sensible-browser https://en.wikipedia.org/wiki/Comedy
+sensible-browser https://en.wikipedia.org/wiki/Speed
+sensible-browser https://en.wikipedia.org/wiki/Clock_rate
+sensible-browser https://en.wikipedia.org/wiki/Microprocessor
+sensible-browser https://en.wikipedia.org/wiki/Command-line_interface
+sensible-browser https://imgflip.com/i/6qyvjx # peek performance 
+sensible-browser https://en.wikipedia.org/wiki/Monitor
+sensible-browser https://imdownwith89.files.wordpress.com/2014/12/nsync_pop.gif # pop gif
+sensible-browser https://frinkiac.com/video/S07E07/paBi_oMVmfiYbQ4eKofB9T-G9EU=.gif #tab gif
+sensible-browser https://www.mozilla.org/en-US/
+sensible-browser https://en.wikipedia.org/wiki/Causality
+sensible-browser https://i.imgur.com/h8KNoLD.gif # correct gif
+sensible-browser https://c.tenor.com/gRjRUZyT4ggAAAAC/fucking-magnets-how-do-they-work-reaction.gif #ICP gif
+sensible-browser https://en.wikipedia.org/wiki/Shebang_\(Unix\)
+sensible-browser https://en.wikipedia.org/wiki/Migraine
+sensible-browser https://en.wikipedia.org/wiki/Synesthesia
+sensible-browser https://en.wikipedia.org/wiki/Johnny_Mnemonic_\(film\)
+sensible-browser https://dirkgently.fandom.com/wiki/Pararibulitis
+sensible-browser https://en.wikipedia.org/wiki/Fibromyalgia
+sensible-browser https://en.wikipedia.org/wiki/American_Sign_Language
+sensible-browser https://en.wikipedia.org/wiki/Alphabet
+sensible-browser https://en.wikipedia.org/wiki/Ultra_high_frequency
+sensible-browser https://en.wikipedia.org/wiki/UHF_\(film\)
+sensible-browser https://media.giphy.com/media/FmSATvDWOLiBq/giphy.gif #UHF gif
+sensible-browser https://www.urbandictionary.com/define.php?term=VPL
+sensible-browser https://www.virtualpilotlight.com/
+sensible-browser https://homestarrunner.com/sbemails/84-kids-book
+sensible-browser https://en.wikipedia.org/wiki/Homestar_Runner
+sensible-browser https://en.wikipedia.org/wiki/Howdy_Doody
+sensible-browser https://www.themarysue.com/community-season-5x09/
+sensible-browser https://www.themarysue.com/wp-content/uploads/2014/03/annieabedhowdycommunity.gif #howdy gif
+sensible-browser https://en.wikipedia.org/wiki/Howdy
+sensible-browser https://en.wikipedia.org/wiki/Facebook
+sensible-browser https://en.wikipedia.org/wiki/YouTube
+sensible-browser https://en.wikipedia.org/wiki/Cybersex_trafficking
+sensible-browser https://en.wikipedia.org/wiki/Streaming_media
+sensible-browser https://en.wikipedia.org/wiki/Livestreaming
+sensible-browser # accompanyment placeholder 
+sensible-browser # placeholder for video link

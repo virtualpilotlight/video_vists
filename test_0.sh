464177c1eac38091aea7ee14325abb64806ac3b0
Omega X Rae <omega.x.rae@gmail.com>
2022-08-23 21:46:19 -0700

    github is stupid and the learning curve it too steep

---
+++ test_0.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# \ is an escap character
+echo what\'s missing\?
+senseible-browser https://en.wikipedia.org/wiki/Jem
+senseible-browser https://www.urbandictionary.com/ 
+echo all about that BASH
+senseible-browser https://www.gnu.org/software/bash/
+senseible-browser https://tiswww.case.edu/php/chet/bash/bashtop.html
+senseible-browser https://www.gnu.org/education/education.html
+senseible-browser https://en.wikipedia.org/wiki/GNU
+senseible-browser https://en.wikipedia.org/wiki/Recursive_acronym
+echo what is the internet
+senseible-browser https://en.wikipedia.org/wiki/Internet_hosting_service
+senseible-browser https://github.com/about
+senseible-browser https://en.wikipedia.org/wiki/GitHub
+senseible-browser https://www.eff.org/
+senseible-browser https://www.wikipedia.org/
+senseible-browser https://www.mozilla.org/en-US/
+senseible-browser https://youtu.be/oaZ9RHZhkuM
+senseible-browser https://youtu.be/nH7aO8r_WR8

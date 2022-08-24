8e0856ba1e92006c15574d26d03d4fa7b504dfb7
virtualpilotlight <omega.x.rae@gmail.com>
2022-08-24 11:14:41 -0700

    a little further with links

--- counter.sh
+++ counter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # counter test bash script
 COUNTER=0
-echo doesn\'t need double quotes but has more formating options
+echo  echo doesn\'t need double quotes but has more formating options
 printf "print the counter %d\n" $COUNTER
 let COUNTER++ 
 printf "print the counter %d\n" $COUNTER
--- video_3.sh
+++ video_3.sh
@@ -1,12 +1,59 @@
 #!/bin/bash
 # this is a comment
-# left off af 2:09 timestamp on video
+# left off af 2:59 timestamp on video
 # track 0 is 0:37
 # track 1 is 3:59 
 # track 2 is 4:03
 # I need like libraries or something anything to sort sets of related data
 COUNTER=0
-
+sensible-browser https://dazedprod.blob.core.windows.net/dazed-prod/1250/6/1256762.gif # hackers gif
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/Hacker_Manifesto
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/Hackers_\(film\)
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/Hacker_\(disambiguation\)
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/Hacker
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wiktionary.org/wiki/hacker
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wiktionary.org/wiki/hacky
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://support.google.com/websearch/answer/10106608?hl=en
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/GitLab
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/Git
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/GitHub
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/Socialization
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/Communication
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/Short
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://64.media.tumblr.com/d76b5002cb766444c202ba6fb80f16be/tumblr_mp63pcsMwE1sowp1do8_250.gifv # practicle magic gif
+printf "%d\n" $COUNTER
+let COUNTER++
+sensible-browser https://en.wikipedia.org/wiki/Program
+printf "%d\n" $COUNTER
+let COUNTER++
 sensible-browser https://www.baeldung.com/linux/bash-script-counter
 printf "%d\n" $COUNTER
 let COUNTER++

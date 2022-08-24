b65756f4342ec199105c509cc8423a423899b7d9
Omega X Rae <omega.x.rae@gmail.com>
2022-08-23 21:54:35 -0700

    test_0.sh & test_0_w.sh updated

--- test_0.sh
+++ test_0.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # \ is an escap character
 echo what\'s missing\?
-senseible-browser https://en.wikipedia.org/wiki/Jem
-senseible-browser https://www.urbandictionary.com/ 
+sensible-browser https://en.wikipedia.org/wiki/Jem
+sensible-browser https://www.urbandictionary.com/ 
 echo all about that BASH
-senseible-browser https://www.gnu.org/software/bash/
-senseible-browser https://tiswww.case.edu/php/chet/bash/bashtop.html
-senseible-browser https://www.gnu.org/education/education.html
-senseible-browser https://en.wikipedia.org/wiki/GNU
-senseible-browser https://en.wikipedia.org/wiki/Recursive_acronym
+sensible-browser https://www.gnu.org/software/bash/
+sensible-browser https://tiswww.case.edu/php/chet/bash/bashtop.html
+sensible-browser https://www.gnu.org/education/education.html
+sensible-browser https://en.wikipedia.org/wiki/GNU
+sensible-browser https://en.wikipedia.org/wiki/Recursive_acronym
 echo what is the internet
-senseible-browser https://en.wikipedia.org/wiki/Internet_hosting_service
-senseible-browser https://github.com/about
-senseible-browser https://en.wikipedia.org/wiki/GitHub
-senseible-browser https://www.eff.org/
-senseible-browser https://www.wikipedia.org/
-senseible-browser https://www.mozilla.org/en-US/
-senseible-browser https://youtu.be/oaZ9RHZhkuM
-senseible-browser https://youtu.be/nH7aO8r_WR8
+sensible-browser https://en.wikipedia.org/wiki/Internet_hosting_service
+sensible-browser https://github.com/about
+sensible-browser https://en.wikipedia.org/wiki/GitHub
+sensible-browser https://www.eff.org/
+sensible-browser https://www.wikipedia.org/
+sensible-browser https://www.mozilla.org/en-US/
+sensible-browser https://youtu.be/oaZ9RHZhkuM
+sensible-browser https://youtu.be/nH7aO8r_WR8
---
+++ test_0_w.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# \ is an escap character
+echo what\'s missing\?
+start firefox https://en.wikipedia.org/wiki/Jem
+start firefox https://www.urbandictionary.com/ 
+echo all about that BASH
+start firefox https://www.gnu.org/software/bash/
+start firefox https://tiswww.case.edu/php/chet/bash/bashtop.html
+start firefox https://www.gnu.org/education/education.html
+start firefox https://en.wikipedia.org/wiki/GNU
+start firefox https://en.wikipedia.org/wiki/Recursive_acronym
+echo what is the internet
+start firefox https://en.wikipedia.org/wiki/Internet_hosting_service
+start firefox https://github.com/about
+start firefox https://en.wikipedia.org/wiki/GitHub
+start firefox https://www.eff.org/
+start firefox https://www.wikipedia.org/
+start firefox https://www.mozilla.org/en-US/
+start firefox https://youtu.be/oaZ9RHZhkuM
+start firefox https://youtu.be/nH7aO8r_WR8

04427cf2f5b8e134e531a2c1cc117b237caf1653
Omega X Rae <omega.x.rae@gmail.com>
2022-08-29 08:25:03 -0700

    new script

---
+++ hacked.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#sensible-browser 
+#let COUNTER++
+#printf "%d\n" $COUNTER
+
+COUNTER=0
+
+count_fun () {
+	let COUNTER++
+	printf "%d\n" $COUNTER
+}
+
+sensible-browser https://media.giphy.com/media/ZMvG5L7Di4AgM/giphy.gif
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Sex_reassignment_surgery
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Sex_assignment
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Secondary_sex_characteristic
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Sexual_dimorphism
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Sexing
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Sex_\(disambiguation\)
+count_fun
+# my browser was closed while I was hacking on this. 
+sensible-browser https://en.wiktionary.org/wiki/agency
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Agency
+count_fun
+sensible-browser https://www.merriam-webster.com/dictionary/masturbatory
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Support
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Behaviorism
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Gender
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Big_Ask
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Branding
+count_fun
+sensible-browser https://knowyourmeme.com/
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Advertising
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Abusive_power_and_control
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Ability
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Cybercrime
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Hacking
+count_fun
+sensible-browser https://library.municode.com/ca/los_angeles_county/codes/code_of_ordinances?nodeId=TIT12ENPR_CH12.08NOCO
+count_fun
+sensible-browser https://www.virtualpilotlight.com/ #other tabs
+count_fun
+sensible-browser https://youtu.be/u4FF6MpcsRw # acoompanyment placehode
+count_fun
+sensible-browser # video place holder
+count_fun

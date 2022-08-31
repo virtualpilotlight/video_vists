5d4de795f551619ea524d635a1e47eac2ffa1c84
virtualpilotlight <omega.x.rae@gmail.com>
2022-08-30 18:41:39 -0700

    Please remember to add the video link to both rent.sh & rent_w.sh

---
+++ rent_w.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+#start firefox 
+#let COUNTER++
+#printf "%d\n" $COUNTER
+
+COUNTER=0
+
+count_fun () {
+	let COUNTER++
+	printf "%d\n" $COUNTER
+}
+
+# 6:03 for track0
+
+start firefox https://en.wikipedia.org/wiki/Oakland,_California
+count_fun
+start firefox https://en.wikipedia.org/wiki/Cyberstalking
+count_fun
+start firefox https://en.wikipedia.org/wiki/Money_\(disambiguation\)
+count_fun
+start firefox https://youtu.be/W3div7N7bT0?t=9
+count_fun
+start firefox https://en.wikipedia.org/wiki/Network_monitoring
+count_fun
+start firefox https://www.urbandictionary.com/define.php?term=rigs
+count_fun
+start firefox https://en.wikipedia.org/wiki/Rig
+count_fun
+start firefox https://www.raspberrypi.org/
+count_fun
+start firefox https://en.wikipedia.org/wiki/Follow-up
+count_fun
+start firefox https://en.wikipedia.org/wiki/Driving
+count_fun
+start firefox https://en.wikipedia.org/wiki/San_Francisco
+count_fun
+start firefox https://en.wikipedia.org/wiki/Video_vixen
+count_fun
+start firefox https://en.wikipedia.org/wiki/Provider
+count_fun
+start firefox https://en.wikipedia.org/wiki/Blue_Suede_Shoes
+count_fun
+start firefox https://en.wikipedia.org/wiki/Pain_management
+count_fun
+start firefox https://media.giphy.com/media/XPpcKKVatItO0/giphy.gif # already did something gif
+count_fun
+start firefox https://media.giphy.com/media/lmv5aDvwOgTmby3a13/giphy.gif # fun gif
+count_fun
+start firefox https://www.hospitalitywifi.com/wifi-university/wifi-interference/
+count_fun
+start firefox https://www.oaklandca.gov/resources/housing-resources-erap-emergency-rental-assistance
+count_fun
+start firefox https://64.media.tumblr.com/c5b689942e420a1b38325e50c8cc9d6b/tumblr_mm1wrlplL21s98pkbo1_250.gif # Guess what... gif
+count_fun
+start firefox https://en.wikipedia.org/wiki/Stalking
+count_fun
+start firefox https://i.imgflip.com/cdy3y.gif # we gon find u gif
+count_fun
+start firefox https://en.wiktionary.org/wiki/no_shit,_Sherlock
+count_fun
+start firefox https://en.wikipedia.org/wiki/YouTube
+count_fun
+start firefox https://en.wikipedia.org/wiki/Obfuscation
+count_fun
+start firefox https://en.wikipedia.org/wiki/Management_\(disambiguation\)
+count_fun
+start firefox https://en.wikipedia.org/wiki/Uncanny
+count_fun
+start firefox https://gifs.cackhanded.net/invader-zim/the-nightmare-begins/stupid.gif
+count_fun
+start firefox https://en.wikipedia.org/wiki/Motion_detection
+count_fun
+start firefox https://en.wikipedia.org/wiki/Stealth
+count_fun
+start firefox https://www.virtualpilotlight.com/
+count_fun
+start firefox https://en.wikipedia.org/wiki/Introduction
+count_fun
+start firefox https://en.wikipedia.org/wiki/Bait-and-switch
+count_fun
+start firefox https://en.wikipedia.org/wiki/The_Hellbound_Heart
+count_fun
+start firefox https://en.wikipedia.org/wiki/Clive_Barker
+count_fun
+start firefox https://en.wikipedia.org/wiki/Brand_relationship
+count_fun
+start firefox https://en.wikipedia.org/wiki/Research_and_development
+count_fun
+start firefox https://en.wikipedia.org/wiki/No
+count_fun
+start firefox https://en.wikipedia.org/wiki/Show_Me_Love_\(t.A.T.u._song\)
+count_fun
+start firefox https://en.wikipedia.org/wiki/Foley_\(filmmaking\)
+count_fun
+start firefox https://en.wikipedia.org/wiki/SpongeBob_SquarePants
+count_fun
+start firefox https://en.wikipedia.org/wiki/Beetlejuice_\(TV_series\)
+count_fun
+start firefox https://en.wikipedia.org/wiki/The_IT_Crowd
+count_fun
+start firefox https://en.wikipedia.org/wiki/Audio_editing_software
+count_fun
+start firefox https://www.urbandictionary.com/define.php?term=can%27t%20even
+count_fun
+start firefox https://en.wikipedia.org/wiki/Just_Shoot_Me!
+count_fun
+start firefox https://media.giphy.com/media/521UkKmXvYZj3gpNqX/giphy.gif # an attempt was made gif
+count_fun
+start firefox https://youtu.be/dvOXuZwOEvM?t=60 # Accompanyment 0 Emilie Autumn - Fight Like A Girl
+count_fun
+start firefox # video place holder
+count_fun

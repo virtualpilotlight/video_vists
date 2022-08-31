24b6b8bcbb88ed7a85f1b5ed5b51cff8b9555531
Omega X Rae <omega.x.rae@gmail.com>
2022-08-31 09:59:34 -0700

    Please remember to add the link the video from the youtube platform

---
+++ untitled.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#sensible-browser 
+#let COUNTER++
+#printf "%d\n" $COUNTER
+
+COUNTER=0
+
+count_fun () {
+	let COUNTER++
+	printf "%d\n" $COUNTER
+}
+
+sensible-browser https://en.wikipedia.org/wiki/Child_abuse
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Psychosis
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Right_to_privacy
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Stalking
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Personal_boundaries
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Alarm_(disambiguation)
+count_fun
+sensible-browser https://www.youtube.com/channel/UCJy3YofcwT_vPyo-EIlHsZg
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Writer%27s_block
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Technical_writing
+count_fun
+sensible-browser https://www.urbandictionary.com/define.php?term=find%20a%20stranger%20in%20the%20alps
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Platform_as_a_service
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Client-side
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Server-side
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Bug_bounty_program
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Subscription_business_model
+count_fun
+sensible-browser https://www.youtube.com/channel/UCEuJw_ZvbTsiOFHfEJg1-xw/featured
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Goth_subculture
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Industrial_music
+count_fun
+sensible-browser https://www.youtube.com/channel/UCzaqS-NuR2_4j043iiN-kWw
+count_fun
+sensible-browser https://en.wiktionary.org/wiki/auto-generated
+count_fun
+sensible-browser https://www.youtube.com/channel/UCIzc1_X3OAFquArF3EOVeWw/featured
+count_fun
+sensible-browser https://youtu.be/VJK7hk9ET8Q  # accompany ment 1
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/User
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Customer_service
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Asset_\(disambiguation\)
+count_fun
+sensible-browser https://en.wikipedia.org/wiki/Database
+count_fun
+sensible-browser https://www.youtube.com/user/Leikeli47VEVO
+count_fun
+sensible-browser https://www.youtube.com/user/ImogenHeapVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/ScissorSistersVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/SpiceGirlsVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/NickiMinajAtVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/TrinaVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/RobynVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/PlaceboVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/AllieXVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/PTAFVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/tATuVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/TLCVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/le1fVEVO
+count_fun
+sensible-browser https://www.youtube.com/user/banksVEVO
+count_fun
+sensible-browser https://www.virtualpilotlight.com/
+count_fun
+sensible-browser https://youtu.be/q7WCmL6U_2s # acoompanyment placehode
+count_fun
+sensible-browser # video place holder
+count_fun

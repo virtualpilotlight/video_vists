255f4c5eb3538e33ed681b4414a44714d205914b
virtualpilotlight <omega.x.rae@gmail.com>
2022-09-01 19:15:03 -0700

    makeup dot mp4 uploading to youtube currently

---
+++ makeup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#sensible-browser 
+#let COUNTER++
+#printf "%d\n" $COUNTER
+
+COUNTER=0
+
+count_fun () {
+	let COUNTER++
+	printf "%d\n" $COUNTER
+}
+
+sensible-browser https://youtu.be/YvcLDewqDi0 # acc 4 
+count_fun
+sensible-browser https://youtu.be/jhC1pI76Rqo # accompanyment 3 
+count_fun
+sensible-browser https://youtu.be/8Ilh1ewceco # acoompanyment 2 3:56 / 9:57
+count_fun
+sensible-browser https://blackphoenixalchemylab.com/shop/general-catalog-perfume-oils/bewitching-brews/odeonmelancholy/
+count_fun
+sensible-browser https://youtu.be/6yP4Nm86yk0 # acompanyment 1 - 3:28 / 6:02
+count_fun
+sensible-browser https://youtu.be/JV-rxGI9eNU # acoompanyment 0 - 2:37
+count_fun
+sensible-browser https://youtu.be/OG0aQUTnKDQ # video place holder
+count_fun
+printf "Does link equel endorcment?"
+printf "from an algorithmic prospective, yes."
+printf "surely, we're in control of the algorithms."
+printf "that depends, who is \"we\"?"

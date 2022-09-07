a2586eba64b25d7796a71c7e87bac997319a4179
Omega X Rae <63996271+virtualpilotlight@users.noreply.github.com>
2022-09-07 16:31:24 -0700

    Create the_game.sh

---
+++ the_game.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COUNTER=0
+
+count_fun () {
+	let COUNTER++
+	printf "%d\n" $COUNTER
+}
+
+sensible-browser https://youtu.be/Ujj7S0EgHFQ # acoompanyment placehode
+count_fun
+sensible-browser https://youtu.be/4g7_i6vVl6A # video place holder
+count_fun

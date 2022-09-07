c3784c83e047d1823299f8e9e5370481f6def33a
Omega X Rae <63996271+virtualpilotlight@users.noreply.github.com>
2022-09-07 16:32:53 -0700

    Update the_game.sh

--- the_game.sh
+++ the_game.sh
@@ -7,6 +7,8 @@ count_fun () {
 	printf "%d\n" $COUNTER
 }
 
+sensible-browse https://www.virtualpilotlight.com/
+count_fun
 sensible-browser https://youtu.be/Ujj7S0EgHFQ # acoompanyment placehode
 count_fun
 sensible-browser https://youtu.be/4g7_i6vVl6A # video place holder
